6324468155fbc31263bfde7769e8b5b1a9308d0c
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-04-23 11:28:20 +1000

    Update HeaderMain.js

--- src/components/Headers/HeaderMain.js
+++ src/components/Headers/HeaderMain.js
@@ -11,13 +11,10 @@ const HeaderMainStyled = styled.div`
 `
 
 const ShopName = styled.image`
-    background: ${props => props.theme.colors.main};
     padding: 20px;
-    width: 100%;
-    background: url("images/card-back.jpg") no-repeat;
+    background: url("content/assets/gatsby-icon.png") no-repeat;
     text-align: center;
     font-family: Heebo, sans-serif;
-    border: 4px solid ${props => props.theme.colors.secondaryAccent};
     font-size: 2.5em;
     font-weight: 900;
     @media (max-width: 600px) {
