b0c5a0e928831d62b8142a0832cf41c1d47fabb4
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-04-23 11:30:59 +1000

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -45,7 +45,7 @@ class Layout extends React.Component {
   render() {
     const { location, children } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
-    const siteName = "Gwen's Cake Shop"
+    const siteName = "Bailey Coffee"
     let header
 
     if (location.pathname === rootPath) {
@@ -68,9 +68,7 @@ class Layout extends React.Component {
             {header}
             <MainSection>{children}</MainSection>
             <FooterStyled>
-              <strong>Gatsby Snipcart Starter
-           - Made by <ExternalLink href="https://www.issydennis.com/" target="_blank" rel="noopener noreferrer">Issy Dennis</ExternalLink>
-              </strong>
+              <strong>2020 Bailey Coffee </strong>
             </FooterStyled>
           </PageWrapper>
         </>
