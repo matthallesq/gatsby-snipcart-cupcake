b8359e499a97046254e4abca702112db619c8afe
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-04-23 14:37:59 +1000

    Update theme.js

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,10 +1,10 @@
-// Here a theme is defined. This theme is passed to the entire site (via layout.js) so the individual parts of the theme object 
+// Here a theme is defined. This theme is passed to the entire site (via layout.js) so the individual parts of the theme object
 // can be accessed in each styled component.
 
 export const theme = {
     colors: {
-        main: '#E2C391', //used in header
-        primaryAccent: '#F6E27F', //used for thumbnails on homepage and as price background
-        secondaryAccent: '#9BBEC7', //used for header border and item buttons
+        main: '#1F3044', //used in header
+        primaryAccent: '#FB9039', //used for thumbnails on homepage and as price background
+        secondaryAccent: '#646C79', //used for header border and item buttons
     }
 };
