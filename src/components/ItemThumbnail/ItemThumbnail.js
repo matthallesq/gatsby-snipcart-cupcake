7fad8728823faeca5eed9b1e9651b0e7f3a60ebb
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-05-19 12:41:09 +1000

    sam

--- src/components/ItemThumbnail/ItemThumbnail.js
+++ src/components/ItemThumbnail/ItemThumbnail.js
@@ -65,7 +65,7 @@ const itemThumbnail = (props) => {
                 <ImgStyled fluid={props.image} />
                 <Heading>{props.heading}</Heading>
             </LinkStyled>
-            <Price>£{props.price.toFixed(2)}</Price>
+            <Price>${props.price.toFixed(2)}</Price>
         </ItemThumbnailStyled >
     )
 }
