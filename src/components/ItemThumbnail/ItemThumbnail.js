06ac0ccf50c7c6ad4e28d632545170f132ff10af
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-05-20 18:43:51 +1000

    Update ItemThumbnail.js

--- src/components/ItemThumbnail/ItemThumbnail.js
+++ src/components/ItemThumbnail/ItemThumbnail.js
@@ -11,6 +11,8 @@ const ItemThumbnailStyled = styled.div`
     align-items: center;
     justify-content: center;
     margin: 10px;
+    padding: 50px;
+
     background: ${props => props.theme.colors.primaryAccent};
 
     @media (max-width: 930px) {
