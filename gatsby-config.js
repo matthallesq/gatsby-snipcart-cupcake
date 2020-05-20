62a541757dc09cf9eba4d6aab5702af984e59764
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-05-20 12:04:57 +1000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/about`,
+        name: `about`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
