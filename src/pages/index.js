e71532f9171f783421c93a39a4d619df7fd86aa4
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-05-21 14:54:06 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,6 +49,18 @@ const ThumbnailsWrapper = styled.div`
     margin-left:20px;
 `
 
+export default function Home() {
+  return (
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
+    </div>
+  )
+}
+
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
