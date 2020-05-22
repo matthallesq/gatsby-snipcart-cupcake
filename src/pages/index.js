43849ce4a3bda6547149935aeae17f5e44b7bdf4
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-05-22 17:28:19 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -103,10 +103,6 @@ class BlogIndex extends React.Component {
 
 const IndexPage = props => (
 
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
     <Img fluid={props.data.imageOne.childImageSharp.fluid} />
  
 );
