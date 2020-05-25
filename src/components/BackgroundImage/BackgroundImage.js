354b63fa5d84b16119b753475bc9ef15ced9026c
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-05-25 15:10:22 +1000

    Update BackgroundImage.js

--- src/components/BackgroundImage/BackgroundImage.js
+++ src/components/BackgroundImage/BackgroundImage.js
@@ -4,24 +4,6 @@ import { Link } from "gatsby";
 import Img from "gatsby-image";
 
 
-const BackgroundImage = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin: 10px;
-    background: ${props => props.theme.colors.primaryAccent};
-
-    @media (max-width: 930px) {
-        width: 250px;
-      }
-
-      @media (max-width: 710px) {
-        width: 100%;
-      }
-`
-
 const ImgStyled = styled(Img)`
     width: 100%;
     height: 350px;
