040cf896d4b382fdf7dbffc1355be5a917fafb92
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-05-27 12:13:22 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,16 @@ const ThumbnailsWrapper = styled.div`
     padding: 10px 20px 20px 20px;
     margin-left:20px;
 
+`const Wrapper = styled.div`
+    width: 100%;
+    height: 60vh; 
+    background-size: cover;
+    background-position: center;
+    background: #646C79;
+    background-repeat: no-repeat;
+    margin-bottom: 10px;
 `
+
 
 
 class BlogIndex extends React.Component {
