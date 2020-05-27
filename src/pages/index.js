9dd9464dd88449ab274502d3584f1927bff823b8
matthallesq <32915847+matthallesq@users.noreply.github.com>
2020-05-27 12:24:37 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import styled from "styled-components"
 import ItemThumbnail from '../components/ItemThumbnail/ItemThumbnail'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import BGImg from "../content/assets/bailey-background.jpg"
+import BGImg from "bailey-background.jpg"
 
 
 const Heading1White = styled.h1`
